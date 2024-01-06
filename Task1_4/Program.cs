ef7af8d9cf8db27202de224cba38ce6f885f0c59
Konstantin <konstbaz@gmail.com>
2024-01-06 23:56:15 +0300

    Сделал что смог, а смог не много по заданию 4

--- Task1_4/Program.cs
+++ Task1_4/Program.cs
@@ -1,3 +1,18 @@
 ﻿//Напишите программу, которая на вход принимает 
 //натуральное число N, а на выходе показывает его цифры через запятую.
 
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите натуральное число N:");
+        int N = int.Parse(Console.ReadLine()!);
+        while (N > 0)
+        {
+        Console.WriteLine(N % 10);
+        N = N / 10; 
+        }   
+    }
+}
